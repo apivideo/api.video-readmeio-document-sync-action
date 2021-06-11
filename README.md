eade512e7290ae5a5c7b01c032a0ba787cfa4b19
Olivier Lando <olivier@api.video>
2021-06-11 10:40:30 +0200

    Use vercel/ncc

--- README.md
+++ README.md
@@ -1,33 +1,38 @@
-# readmeio-document-sync-action
-This action let you update a readme.io documentation (for instance using your repository readme).
+# apivideo/readmeio-document-sync-action
+This action let you synchronize a readme.io documentation using a markdown file from your repository.
 
 
 ## Inputs
 
-### `document-slug`
+#### `document-slug`
 
 **Required** Slug of the readme.io document to synchronize.
 
-### `content`
+#### `markdown-file-path`
 
-**Required** The content of the document.
+**Required** The path of the markdown file to use for synchronization (eg. `README.md`).
 
-### `readme-io-api-key
+#### `readme-io-api-key`
 
 **Required** Your API key to request readme.io.
 
-### `relative-links-host`
+#### `make-relative-links-absolute`
 
-If provided, all relative markdown links in the document will be prefixed with this value.
+If true, all relative markdown links in the markdown will be made absolute (default: false).
 
 ## Outputs
 
-### `response`
+#### `response`
 
 The status code of the response from readme.io.
 
 ## Example usage
 
-uses: apivideo/hello-world-javascript-action@v1.1
+```yml
+uses: apivideo/readmeio-document-sync-action
 with:
-  who-to-greet: 'Mona the Octocat'
+  document-slug: 'my-software-documentation'
+  markdown-file-path: README.md
+  make-relative-links-absolute: true
+  readme-io-api-key: ${{ secrets.README_IO_API_KEY }}
+```
--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+const path = require("path");
+const request = require('request');
+
+const makeLinksAbsolute = (repository, markdownFilePath, content) => {
+    const relativeFolder = path.parse(markdownFilePath).dir;
+    const linksPrefix = `${repository.html_url}/blob/${repository.default_branch}/${!!relativeFolder ? relativeFolder + '/' : ''}`;
+
+    return content.replace(/\[([^\[]+)\]\(((?!https?:\/\/).*)\)/gm, `[$1](${linksPrefix}$2)`);
+}
+
+const uploadDocument = (content, slug, apiKey, callback) => {
+    const options = {
+        method: 'PUT',
+        url: `https://dash.readme.com/api/v1/docs/${slug}`,
+        headers: {
+            Authorization: `Basic ${apiKey}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            //  excerpt: description,
+            body: content
+        })
+    };
+
+    request(options, callback);
+}
 
 try {
-    const documentSlug = core.getInput('document-slug');
-    const content = core.getInput('content');
-    const apiKey = core.getInput('readme-io-api-key');
-    const relativeLinksHost = core.getInput('relative-links-host');
+    const documentSlug = core.getInput('document-slug', { required: true });
+    const markdownFilePath = core.getInput('markdown-file-path', { required: true });
+    const apiKey = core.getInput('readme-io-api-key', { required: true });
+    const makeRelativeLinksAbsolute = core.getInput('make-relative-links-absolute', { trimWhitespace: true }).toLowerCase() === 'true';
 
-    console.log(github.context);
-    console.log({ documentSlug, content, apiKey, relativeLinksHost });
+    const repository = github.context.payload.repository;
 
-    core.setOutput("response", "200");
+    let content = fs.readFileSync(markdownFilePath.trim(), 'utf8');
+
+    if (makeRelativeLinksAbsolute) {
+        content = makeLinksAbsolute(repository, markdownFilePath, content);
+    }
+
+    uploadDocument(content, documentSlug, apiKey, (error, response) => {
+        if (error) {
+            core.setFailed(error);
+            return;
+        };
+        core.setOutput("response", response.statusCode);
+    });
+
 } catch (error) {
     core.setFailed(error.message);
 }
